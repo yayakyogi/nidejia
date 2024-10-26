74f95287415e64e6f1dc928750ab1ed7c179201f
yayakyogi <yayaktaka@gmail.com>
2024-10-26 19:54:20 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#FF9357",
+        black: "#141414",
+        black2: "#202020",
+        grey: "#323437",
+        white: "#FFFFFF",
+        slate: "#A8A8AB",
+      },
+      borderRadius: {
+        20: "20px",
+      },
+      lineHeight: {
+        45: "45px",
+      },
+    },
+  },
+  plugins: [],
+};
